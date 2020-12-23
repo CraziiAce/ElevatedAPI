10f4f9b0746740a0805880da818ed90344ad88d0
CraziiAce <teddyjraz@gmail.com>
2020-12-22 17:11:11 -0700

    try to use url from param

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from PIL import Image
+from urllib.request import urlopen
 
 def communism(request):
-    img = Image.open("api/images/soviet.jpg")
+    url = request.GET['image_url']
+    img = Image.open(urlopen(url))
     back = Image.open("api/images/photo.jpg")
     back = back.resize(img.size)
     blended_image = Image.blend(img, back, 0.5)
     blended_image.show()
-    response = HttpResponse("dope")
+    response = HttpResponse(content_type="image/jpeg")
+    blended_image.save(response, "JPEG")
     return response
+
